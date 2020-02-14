ecaa09de2f9d9a3304d7cdcb046802243ac3393f
tmartine <thomas.martinet@utbm.fr>
2020-02-14 09:18:22 +0100

    Work on irms function

--- main.c
+++ main.c
@@ -16,6 +16,16 @@
 #define R1 48000.0
 #define R2 4400.0
 
+#define ADC_BITS 24
+#define ADC_COUNTS  (1<<ADC_BITS)
+
+UDOUBLE sampleI;
+UDOUBLE offsetI;
+UDOUBLE filteredI;
+UDOUBLE sumI;
+UDOUBLE sqI;
+UDOUBLE ICAL = 60.0;
+
 void  Handler(int signo)
 {
     //System Exit
@@ -23,6 +33,34 @@ void  Handler(int signo)
     DEV_ModuleExit();
 
     exit(0);
+}
+
+double calcIrms(unsigned int Number_of_Samples, UBYTE pin)
+{
+    for (unsigned int n = 0; n < Number_of_Samples; n++)
+    {
+        sampleI = ADS1256_GetChannalValue(pin);
+
+        // Digital low pass filter extracts the 2.5 V or 1.65 V dc offset,
+        //  then subtract this - signal is now centered on 0 counts.
+        offsetI = (offsetI + (sampleI-offsetI)/1024);
+        filteredI = sampleI - offsetI;
+
+        // Root-mean-square method current
+        // 1) square current values
+        sqI = filteredI * filteredI;
+        // 2) sum
+        sumI += sqI;
+    }
+
+    double I_RATIO = ICAL *((5.0/1000.0) / (ADC_COUNTS));
+    Irms = I_RATIO * sqrt(sumI / Number_of_Samples);
+
+    //Reset accumulators
+    sumI = 0;
+    //--------------------------------------------------------------------------------------
+
+    return Irms;
 }
 
 int main(void)
@@ -60,6 +98,8 @@ int main(void)
         printf("A1=%f\r\n",a1);
         printf("A2=%f\r\n",a2);
         printf("A3=%f\r\n",a3);
+        float ax = calcIrms(1480, SCT013_1);
+        printf("AX=%f\r\n",ax);
         printf("\r\n\r\n");
 
         // Adapt voltage
