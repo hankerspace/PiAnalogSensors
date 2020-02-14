3f7a2dbc98efdc20e04c1cb39ee5b4c276e0006d
tmartine <thomas.martinet@utbm.fr>
2020-02-14 21:06:58 +0100

    Work on current avg

--- main.c
+++ main.c
@@ -19,12 +19,6 @@
 #define ADC_BITS 24
 #define ADC_COUNTS  (1<<ADC_BITS)
 
-UDOUBLE sampleI;
-UDOUBLE offsetI;
-UDOUBLE filteredI;
-UDOUBLE sumI;
-UDOUBLE sqI;
-UDOUBLE ICAL = 60.0;
 
 void  Handler(int signo)
 {
@@ -33,35 +27,6 @@ void  Handler(int signo)
     DEV_ModuleExit();
 
     exit(0);
-}
-
-double calcIrms(unsigned int Number_of_Samples, UBYTE pin)
-{
-    double Irms;
-    for (unsigned int n = 0; n < Number_of_Samples; n++)
-    {
-        sampleI = ADS1256_GetChannalValue(pin);
-
-        // Digital low pass filter extracts the 2.5 V or 1.65 V dc offset,
-        //  then subtract this - signal is now centered on 0 counts.
-        offsetI = (offsetI + (sampleI-offsetI)/1024);
-        filteredI = sampleI - offsetI;
-
-        // Root-mean-square method current
-        // 1) square current values
-        sqI = filteredI * filteredI;
-        // 2) sum
-        sumI += sqI;
-    }
-
-    double I_RATIO = ICAL *((5.0/1000.0) / (ADC_COUNTS));
-    Irms = I_RATIO * sqrt(sumI / Number_of_Samples);
-
-    //Reset accumulators
-    sumI = 0;
-    //--------------------------------------------------------------------------------------
-
-    return Irms;
 }
 
 int main(void)
@@ -81,15 +46,25 @@ int main(void)
         exit(0);
     }
     
-    
+    long count = 0;
+    double sum = 0;
     while(1){
+        count++;
 
         ADS1256_GetAll(ADC);
-        for(i=0;i<8;i++){
-            //printf("%d %f\r\n",i,ADC[i]*5.0/0x7fffff);
+        for(i=0;i<8;i++) {
+            printf("%d %f\r\n", i, ADC[i] * 5.0 / 0x7fffff);
         }
+        sum +=  ADC[0] * 5.0 / 0x7fffff;
 
-        float v1 = (ADC[V_1]*5.0/0x7fffff) / (R2/(R1+R2));
+        if(count % 1000 == 0) {
+            printf("SUM : %f  \r\n", sum / 1000.0f);
+            sum = 0;
+        }
+        printf("\33[9A");//Move the cursor up 8 lines
+
+
+        /*float v1 = (ADC[V_1]*5.0/0x7fffff) / (R2/(R1+R2));
         float v2 = (ADC[V_2]*5.0/0x7fffff) / (R2/(R1+R2));
         printf("V1=%f\r\n",v1);
         printf("V2=%f\r\n",v2);
@@ -99,16 +74,16 @@ int main(void)
         printf("A1=%f\r\n",a1);
         printf("A2=%f\r\n",a2);
         printf("A3=%f\r\n",a3);
-        /*float ax = calcIrms(1480, SCT013_1);
-        printf("AX=%f\r\n",ax);*/
-        printf("\r\n\r\n");
+        float ax = calcIrms(1480, SCT013_1);
+        printf("AX=%f\r\n",ax);
+        printf("\r\n\r\n");*/
 
         // Adapt voltage
         x = (ADC[0] >> 7)*5.0/0xffff;
         DAC8532_Out_Voltage(channel_B, (x));
         DAC8532_Out_Voltage(channel_A, (3.3 - x));
 
-        sleep(1);
+        //sleep(1);
 
     }
     return 0;
